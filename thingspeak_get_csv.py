76af5062cb393074e44a19b4f5c9d0cd624187ba
Daniel Nowak <38864627+DanielNowak98@users.noreply.github.com>
2021-12-15 14:30:01 +0100

    Update thingspeak_get_csv.py

--- thingspeak_get_csv.py
+++ thingspeak_get_csv.py
@@ -1,11 +1,4 @@
 import requests as rq
 
-import pandas as pd
-
-
 r = rq.get('https://api.thingspeak.com/channels/1580340/feeds.csv?api_key=04C8XY3Q93P9TM12', allow_redirects=True)
 open('filename.csv', 'wb').write(r.content)
-
-df = pd.read_csv("filename.csv")
-
-print(df.head())
